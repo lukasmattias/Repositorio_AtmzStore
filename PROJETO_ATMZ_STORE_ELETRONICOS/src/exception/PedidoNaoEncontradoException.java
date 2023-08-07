850535aaba459b4a31cf9c7efcc941de75ca3cdf
Mattias <99132374+lukasmattias@users.noreply.github.com>
2023-08-07 09:24:47 -0300

    Delete PROJETO_ATMZ_STORE_ELETRONICOS directory

--- PROJETO_ATMZ_STORE_ELETRONICOS/src/exception/PedidoNaoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class PedidoNaoEncontradoException extends RuntimeException {
-    public PedidoNaoEncontradoException() {
-        super("Pedido não encontrado.");
-    }
-}
