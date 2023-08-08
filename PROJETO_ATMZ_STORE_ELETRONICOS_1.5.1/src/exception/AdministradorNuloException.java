a0684b7b0ee3d9cd2a825181fcf621d2337e1f97
jeanevitoria <131841615+jeanevitoria@users.noreply.github.com>
2023-08-07 22:52:55 -0300

    Delete PROJETO_ATMZ_STORE_ELETRONICOS_1.5.1 directory

--- PROJETO_ATMZ_STORE_ELETRONICOS_1.5.1/src/exception/AdministradorNuloException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class AdministradorNuloException extends RepositorioException {
-    public AdministradorNuloException() {
-        super("Administrador não pode ser nulo.");
-    }
-}
