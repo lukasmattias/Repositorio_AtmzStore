5063ab1ab41c10150447811cb5af1f57451a0f01
jeanevitoria <131841615+jeanevitoria@users.noreply.github.com>
2023-08-09 05:08:01 -0300

    Delete PROJETO_ATMZ_STORE_ELETRONICOS directory

--- PROJETO_ATMZ_STORE_ELETRONICOS/src/exception/PagamentoNaoDefinidoException.java
+++
@@ -1,8 +0,0 @@
-package exception;
-
-public class PagamentoNaoDefinidoException extends RuntimeException {
-    public PagamentoNaoDefinidoException() {
-        super("Pagamento nao esta definido para este pedido.");
-    }
-}
-
