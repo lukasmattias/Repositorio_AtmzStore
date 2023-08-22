be633f22241078f70cc95671336864ba07793179
Lucas-X-A <140546986+Lucas-X-A@users.noreply.github.com>
2023-08-22 18:14:51 -0300

    AdmVendasController.java

--- PROJETO_ATMZ_STORE_ELETRONICOS/src/gui/AdmVendasController.java
+++ PROJETO_ATMZ_STORE_ELETRONICOS/src/gui/AdmVendasController.java
@@ -21,7 +21,7 @@ public class AdmVendasController {
 
     @FXML
     void VoltarLogin(ActionEvent event)  throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setTitle("ATMZ STORE");
