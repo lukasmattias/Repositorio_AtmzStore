b4f6bf8f485c21e4877d52fde22a7c454753d999
Lucas-X-A <140546986+Lucas-X-A@users.noreply.github.com>
2023-08-22 17:51:00 -0300

    ClienteComprasController.java

---
+++ PROJETO_ATMZ_STORE_ELETRONICOS/src/gui/ClienteComprasController.java
@@ -0,0 +1,31 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class ClienteComprasController {
+
+    private Stage stage;
+    private Scene scene;
+    @FXML
+    private Button btnVoltarCarrinho;
+
+    @FXML
+    void VoltarCarrinho(ActionEvent event)  throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/Carrinho.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setTitle("ATMZ STORE");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
