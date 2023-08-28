a3a5d0445a3ca736165921ddad251fd60213c5ba
Lucas-X-A <140546986+Lucas-X-A@users.noreply.github.com>
2023-08-28 13:46:41 -0300

    ProdutoAdicionarController.java

--- PROJETO_ATMZ_STORE_ELETRONICOS/src/gui/ProdutoAdicionarController.java
+++ PROJETO_ATMZ_STORE_ELETRONICOS/src/gui/ProdutoAdicionarController.java
@@ -29,130 +29,128 @@ import negocio.beans.Produto;
 
 public class ProdutoAdicionarController {
 
-	@FXML
-	private MenuItem ProdutoAdicionarComputadores;
+    @FXML
+    private MenuItem ProdutoAdicionarComputadores;
 
-	@FXML
-	private TextField ProdutoAdicionarDescricao;
+    @FXML
+    private TextField ProdutoAdicionarDescricao;
 
-	@FXML
-	private MenuItem ProdutoAdicionarEletrodomesticos;
+    @FXML
+    private MenuItem ProdutoAdicionarEletrodomesticos;
 
-	@FXML
-	private Spinner<Integer>ProdutoAdicionarEstoque;
+    @FXML
+    private Spinner<Integer>ProdutoAdicionarEstoque;
 
-	@FXML
-	private MenuItem ProdutoAdicionarFotoVideo;
+    @FXML
+    private MenuItem ProdutoAdicionarFotoVideo;
 
-	@FXML
-	private MenuItem ProdutoAdicionarGadgets;
+    @FXML
+    private MenuItem ProdutoAdicionarGadgets;
 
-	@FXML
-	private TextField ProdutoAdicionarNome;
+    @FXML
+    private TextField ProdutoAdicionarNome;
 
-	@FXML
-	private TextField ProdutoAdicionarPreco;
+    @FXML
+    private TextField ProdutoAdicionarPreco;
 
-	@FXML
-	private MenuItem ProdutoAdicionarSmartphones;
+    @FXML
+    private MenuItem ProdutoAdicionarSmartphones;
 
-	@FXML
-	private Button btnProdutoAdicionar;
+    @FXML
+    private Button btnProdutoAdicionar;
 
-	@FXML
-	private Button btnProdutoAdicionarCancelar;
+    @FXML
+    private Button btnProdutoAdicionarCancelar;
 
-	@FXML
+    @FXML
     private MenuButton menuProdutoAdicionarCategoria;
-	
-	Categoria aux;
-	
-	
-	public void initialize() {
-		SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, Integer.MAX_VALUE, 0);
+
+    //Categoria aux ;
+    int aux;
+
+    public void initialize() {
+        SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, Integer.MAX_VALUE, 0);
         ProdutoAdicionarEstoque.setValueFactory(valueFactory);
-        
+
         permitirApenasNumeros(ProdutoAdicionarPreco);
-        
+
     }
 
-	
-	@FXML
-	void AdicionarProduto(ActionEvent event) throws IOException {
-		String nome = ProdutoAdicionarNome.getText();
-		String descricao = ProdutoAdicionarDescricao.getText();
-		double preco = Float.parseFloat(ProdutoAdicionarPreco.getText());
-		int estoque = ProdutoAdicionarEstoque.getValue();
-		Categoria categoria = aux;
-	
-		 try {
-             ProdutoController.getInstancia().cadastrarProduto(new Produto(nome, descricao, preco, estoque, categoria));
-             Alert alert = new Alert(AlertType.CONFIRMATION);
-             alert.setTitle("Cadastro Concluído");
-             alert.setHeaderText("Cadastro realizado com sucesso!");
-             alert.showAndWait();
-             Stage stage = (Stage) ProdutoAdicionarNome.getScene().getWindow();
-             stage.close();
-         }
-         catch (OperacaoInvalidaException e) {
-             Alert alert = new Alert(AlertType.ERROR);
-             alert.setTitle("Erro de Cadastro");
-             alert.setHeaderText("Ocorreu um erro durante o cadastro:");
-             alert.setContentText(e.getMessage());
-             alert.showAndWait();
-         }
-		 catch (RuntimeException e) {
-             Alert alert = new Alert(AlertType.ERROR);
-             alert.setTitle("Erro de Cadastro");
-             alert.setHeaderText("Ocorreu um erro durante o cadastro:");
-             alert.setContentText(e.getMessage());
-             alert.showAndWait();
-         }
-		 
-	}
 
-	@FXML
-	void FecharTelaProdutoAdicionar(ActionEvent event) {
+    @FXML
+    void AdicionarProduto(ActionEvent event) throws IOException {
+        String nome = ProdutoAdicionarNome.getText();
+        String descricao = ProdutoAdicionarDescricao.getText();
+        double preco = Float.parseFloat(ProdutoAdicionarPreco.getText());
+        int estoque = ProdutoAdicionarEstoque.getValue();
+        Categoria categoria = CategoriaController.getInstancia().buscarCategoriaPorId(aux);
 
-	}
+        try {
+            ProdutoController.getInstancia().cadastrarProduto(new Produto(nome, descricao, preco, estoque, categoria));
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Cadastro Concluído");
+            alert.setHeaderText("Cadastro realizado com sucesso!");
+            alert.showAndWait();
+            Stage stage = (Stage) ProdutoAdicionarNome.getScene().getWindow();
+            stage.close();
+        }
+        catch (OperacaoInvalidaException e) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Erro de Cadastro");
+            alert.setHeaderText("Ocorreu um erro durante o cadastro:");
+            alert.setContentText(e.getMessage());
+            alert.showAndWait();
+        }
 
-	@FXML
-	public void RetornarCategoriaComputadores(ActionEvent event) {
-		menuProdutoAdicionarCategoria.setText("Computadores");
-		aux  = CategoriaController.getInstancia().buscarCategoriaPorId(3);
-	}
+    }
 
-	@FXML
-	public void RetornarCategoriaEletrodomesticos(ActionEvent event) {
-		menuProdutoAdicionarCategoria.setText("Eletrodomesticos");
-		aux = CategoriaController.getInstancia().buscarCategoriaPorId(4);
-	}
+    @FXML
+    void FecharTelaProdutoAdicionar(ActionEvent event) {
 
-	@FXML
-	public void RetornarCategoriaFotoVideo(ActionEvent event) {
-		menuProdutoAdicionarCategoria.setText("Foto e Vídeo");
-		aux = CategoriaController.getInstancia().buscarCategoriaPorId(5);
-	}
+    }
 
-	@FXML
-	public void RetornarCategoriaGadgets(ActionEvent event) {
-		menuProdutoAdicionarCategoria.setText("Gadgets");
-		aux = CategoriaController.getInstancia().buscarCategoriaPorId(2);
-	}
+    @FXML
+    public void RetornarCategoriaComputadores(ActionEvent event) {
+        menuProdutoAdicionarCategoria.setText("Computadores");
+        //aux  = CategoriaController.getInstancia().buscarCategoriaPorId(3);
+        aux = 3;
+    }
 
-	@FXML
-	public void RetornarCategoriaSmartphones(ActionEvent event) {
-		menuProdutoAdicionarCategoria.setText("Smartphones");
-		aux = CategoriaController.getInstancia().buscarCategoriaPorId(1);
-	}
+    @FXML
+    public void RetornarCategoriaEletrodomesticos(ActionEvent event) {
+        menuProdutoAdicionarCategoria.setText("Eletrodomesticos");
+        //aux = CategoriaController.getInstancia().buscarCategoriaPorId(4);
+        aux = 4;
+    }
 
-	
-	private void permitirApenasNumeros(TextField textField) {
-	    textField.textProperty().addListener((observable, valorAntigo, valorNovo) -> {
-	        if (!valorNovo.matches("\\d*([.]\\d*)?")) {
-	            textField.setText(valorNovo.replaceAll("[^\\d.]", ""));
-	        }
-	    });
-	}
-	
+    @FXML
+    public void RetornarCategoriaFotoVideo(ActionEvent event) {
+        menuProdutoAdicionarCategoria.setText("Foto e Vídeo");
+        //aux = CategoriaController.getInstancia().buscarCategoriaPorId(5);
+        aux = 5;
+    }
+
+    @FXML
+    public void RetornarCategoriaGadgets(ActionEvent event) {
+        menuProdutoAdicionarCategoria.setText("Gadgets");
+        //aux = CategoriaController.getInstancia().buscarCategoriaPorId(2);
+        aux = 2;
+    }
+
+    @FXML
+    public void RetornarCategoriaSmartphones(ActionEvent event) {
+        menuProdutoAdicionarCategoria.setText("Smartphones");
+        //aux = CategoriaController.getInstancia().buscarCategoriaPorId(1);
+        aux = 1;
+    }
+
+
+    private void permitirApenasNumeros(TextField textField) {
+        textField.textProperty().addListener((observable, valorAntigo, valorNovo) -> {
+            if (!valorNovo.matches("\\d*([.]\\d*)?")) {
+                textField.setText(valorNovo.replaceAll("[^\\d.]", ""));
+            }
+        });
+    }
+
 }
